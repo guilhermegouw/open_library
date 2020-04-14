7c1fb9a148cabcb625779d14a60bb80becda93c3
guilhermegouw <guilherme.gouw@gmail.com>
2020-04-14 08:03:57 -0300

    colocando excessões

--- open_library.py
+++ open_library.py
@@ -1,13 +1,15 @@
 import requests
 import json
 
-base_url = 'http://openlibrary.org/'
-subject_python = 'search.json?subject=python'
 
-r = requests.get(base_url + subject_python)
+def get_json_result():
+    base_url = 'http://openlibrary.org/search.json?subject=python'
+    try:
+        r = requests.get(base_url)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+    return r.json()
 
-dict_result = r.json()
-json_result = json.dumps(dict_result)
 
-with open('json_result', 'w') as json_file:
-    json.dump(json_result, json_file)
+with open('json_result.json', 'w') as json_file:
+    json.dump(get_json_result(), json_file)
