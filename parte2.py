d3481bb40b264345c6b53d4bbe1848cbc02f6e2d
guilhermegouw <guilherme.gouw@gmail.com>
2020-04-17 13:13:47 -0300

    changes to make it more robust.

--- parte2.py
+++ parte2.py
@@ -21,22 +21,30 @@ with open('json_result.json', 'w') as json_file:
 # Create AWS S3 Bucket
 def create_bucket(bucket_name, region=None):
     try:
-        if region is None:
-            s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket=bucket_name)
-        else:
-            s3_client = boto3.client('s3', region_name=region)
-            location = {'LocationConstraint': region}
-            s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+        s3_client = boto3.client('s3', region_name=region)
+        location = {'LocationConstraint': region}
+        s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
     except ClientError as e:
         logging.error(e)
         return False
     return True
 
 
-create_bucket('open-library', 'sa-east-1')
+s3 = boto3.resource('s3')
+bucket_exists = s3.Bucket('open-library') in s3.buckets.all()
 
-# Upload the file to AWS S3 Bucket
-s3 = boto3.client('s3')
-with open("json_result.json", "rb") as f:
-    s3.upload_fileobj(f, "open-library", "json_result.json")
+if bucket_exists:
+    try:
+        s3.Object('open-library', 'json_result.json').load()
+        print("File already exists inside this bucket.")
+    except ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            s3_client = boto3.client('s3')
+            with open("json_result.json", "rb") as f:
+                s3_client.upload_fileobj(f, "open-library", "json_result.json")
+else:
+    s3_client = boto3.client('s3')
+    create_bucket('open-library', 'sa-east-1')
+    with open("json_result.json", "rb") as f:
+        s3_client.upload_fileobj(f, "open-library", "json_result.json")
+        print("File successfully uploaded.")
